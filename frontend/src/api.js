802fbce45e0b68931eb8d97de2badcb8f2b58a5e
shafiqahmed-786 <shafiqahmed0673@gmail.com>
2026-03-02 15:57:24 +0530

    Connect frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://factory-mlops-dashboard.onrender.com";
 
 export const getMetrics = () =>
   axios.get(`${API}/api/v1/metrics`);
